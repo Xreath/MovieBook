3ecd1b701ef50a5790cadbf6ded9cc86d2ab561a
Xreath <samed_fazli@hotmail.com>
2022-10-03 19:23:15 +0300

    Initial Commit

---
+++ MovieBook/MovieBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookApp.swift
+//  MovieBook
+//
+//  Created by Fazlı Koç on 3.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
